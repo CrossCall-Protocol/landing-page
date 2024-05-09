37f055638a79afc289cf9ec051663b8eff950506
FudgyDRS <yetimonsters@gmail.com>
2024-05-09 18:23:56 -0500

    background done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import logo from './crosscall-logo.svg';
 import './App.css';
-
-// Import the image URL
-const backgroundImageUrl = 'https://fauux.neocities.org/bg/bgN6.gif';
+import './Background2.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <div>Logo</div>
-      <div>Company slogan</div>
-      <div>whitepaper</div>
-      <div>docs</div>
-      <div>
-        <div>Signup Box</div>
-        <div>SIgnup Test</div>
+    <div>
+      <div className="Background">
+        <div className="dark">
+          <div className="darker">
+            <div className="opacity2"></div>
+          </div>
+        </div>
+      </div>
+      {/*  */}
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <div className="landing-logo">Logo</div>
+        <div>Company slogan</div>
+        <div>whitepaper</div>
+        <div>docs</div>
         <div>
-          <div>Gitbook</div>
-          <div>Twitetr</div>
+          <div>Signup Box</div>
+          <div>Signup Test</div>
+          <div>
+            <div>Gitbook</div>
+            <div>Github</div>
+            <div>Twitetr</div>
+          </div>
         </div>
       </div>
     </div>
