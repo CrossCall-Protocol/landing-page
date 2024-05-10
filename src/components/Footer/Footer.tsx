5224139d56bc1769d9cc97cac7ec20fbc5f426bf
FudgyDRS <yetimonsters@gmail.com>
2024-05-09 21:10:29 -0500

    footer fixed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,12 @@
 import React, { useEffect } from 'react';
 import './Footer.css';
 import { ReactComponent as Logo } from '../../assets/svg/logo.svg';
-// import { Swap } from '../Swap/Swap';
+
+import github from '../../assets/svg/github-icon.svg';
+// import telegram from '../../assets/svg/gitbook-icon.svg';
+// import discord from '../../assets/svg/discord-logo.svg';
+import xtwitter from '../../assets/svg/xtwitter-icon.svg';
+import gitbook from '../../assets/svg/gitbook-icon.svg';
 
 export const Footer = () => {
   const handleOnSwap = () => {
@@ -12,9 +17,17 @@ export const Footer = () => {
     <div className="footer">
       <div className="footer-copyright">© 2024 CrossCall Labs Inc.</div>
       <div className="footer-socials">
-        <div className="footer-socials-gitbook">Gitbook</div>
-        <div className="footer-socials-github">Github</div>
-        <div className="footer-socials-x">X</div>
+        <div className="footer-socials-inner">
+          <a href={'github.com'} target="_blank" rel="noopener noreferrer">
+            <img src={github} alt="Github"/>
+          </a>
+          <a href={'github.com'} target="_blank" rel="noopener noreferrer">
+            <img src={gitbook} alt="Gitbook"/>
+          </a>
+          <a href={'github.com'} target="_blank" rel="noopener noreferrer">
+            <img src={xtwitter} alt="X"/>
+          </a>
+        </div>
       </div>
     </div>
   );
