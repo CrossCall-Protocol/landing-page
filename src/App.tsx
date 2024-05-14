bcc21b15a779421d5ab1e0556999e894a3b41c38
FudgyDRS <yetimonsters@gmail.com>
2024-05-14 18:48:18 -0500

    background and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from "react";
-import logo from "./crosscall-logo.svg";
-import "./App.css";
-import "./Background2.css";
-import { Footer } from "./components/Footer/Footer";
-import "./assets/fonts/Quicksand.ttf";
-import { Header } from "./components/Header/Header";
-import { Signup } from "./components/Signup/Signup";
+import React from 'react';
+import logo from './crosscall-logo.svg';
+import './App.css';
+import './Background2.css';
+import { Footer } from './components/Footer/Footer';
+import './assets/fonts/Quicksand.ttf';
+import { Header } from './components/Header/Header';
+import { Signup } from './components/Signup/Signup';
 
 function App() {
   return (
     <div>
-      <div className="Background">
-        <div className="dark">
-          <div className="darker">
-            <div className="opacity2"></div>
+      <div className="outer-bg">
+        <div className="Background">
+          <div className="dark">
+            <div className="darker">
+              <div className="opacity2"></div>
+            </div>
           </div>
         </div>
       </div>
       {/*  */}
       <div className="App">
-        <Header/>
-        <img src={logo} className="landing-logo" alt="logo" />
+        <div className="logo-container">
+          <div className="rainbow-layer"></div>
+          <img src={logo} className="landing-logo" alt="logo" />
+        </div>
+
+        <div className="header-box">
+          <Header />
+        </div>
+
         {/* <div className="landing-slogan">Company slogan</div> */}
 
         <div>
